68b9efc62c772eda85e4c6590bae0c1c9d034a45
Ólavur Mortensen <olavurmortensen@gmail.com>
2018-10-25 11:25:56 +0100

    Moved calc_depth to a separate file as it is not very relevant at the moment.

--- lineages.py
+++ lineages.py
@@ -38,46 +38,6 @@ def csv2dict(csv):
     return dd
 
 
-def calc_depth(ind, gen, d=0, depth=0):
-    '''
-    Calculate the total generational depth of the lineage of a single individual.
-
-    Example:
-        depth = calc_depth(1, gen)
-
-    Input:
-        ind:        Integer, ID of individual.
-        gen:        Dictionary, genealogy object.
-        d:          Integer, current generational depth, do not change [0].
-        depth:      Integer, total generational depth, do not change [0].
-
-    Returns:
-    '''
-
-    # Get the record corresponding to the individual.
-    rec = gen.get(ind)
-
-    assert rec is not None, 'Individual %d does not exist in genealogy.' % ind
-
-    # If current depth is larger than the total, update depth.
-    if d > depth:
-        depth = d
-
-    # Get IDs of parents.
-    fa = rec.fa
-    mo = rec.mo
-
-    # If the parent exists (ID different from 0), make a recursive call.
-    # Pass all parameters on in the recursive call, and increment the depth.
-    # The recursive call will return the resulting generational depth.
-    if fa != 0 and gen.get(fa) is not None:
-        depth = calc_depth(fa, gen, d+1, depth)
-    if mo != 0 and gen.get(mo) is not None:
-        depth = calc_depth(mo, gen, d+1, depth)
-
-    return depth
-
-
 def lineage(ind, gen, lin, depth=None, d=0, by=None):
     '''
     Generate lineage of a single individual based on complete genealogy available.
