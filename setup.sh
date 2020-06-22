c69637ab9ebfb16b7e5bae9e8f6e3d061f233922
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-06-22 14:39:11 +0000

    Moved all scripts (except setup.sh and tests.py) to the 'scripts' folder. Updated setup.sh accordingly.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ged2csv_path=$(pwd)/ged2csv
-lineages_path=$(pwd)/lineages
-export PATH=$lineages_path:$PATH
-export PATH=$ged2csv_path:$PATH
+scripts_path=$(pwd)/scripts
+export PATH=$scripts_path:$PATH
 
